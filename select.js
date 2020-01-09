2e3c936d5a835a3485239f23cdaf36c515dd6b2a
Pandurangan D <pandurangan.d@mrcooper.com>
2020-01-09 12:30:39 +0530

    port added

---
+++ select.js
@@ -0,0 +1,46 @@
+var http = require('http');
+var formidable = require('formidable');
+const csv = require('csv-parser');
+const fs = require('fs');
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+console.log(process.env.PORT);
+
+http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+        const form = new formidable.IncomingForm();
+        const data = [];
+        const header = [];
+
+        form.parse(req, function (err, fields, files) {
+            fs.createReadStream(files.filetoupload.path)
+            .pipe(csv())
+            .on('headers', (headers) => {
+                headers.forEach(element => {
+                    header.push({id: element, title: element})
+                });
+            })
+            .on('data', (row) => {
+                Object.keys(row).forEach(function(key) {
+                    row[key] = row[key].replace(/,/g, '');
+                });
+                data.push(row);
+            })
+            .on('end', () => {
+                const csvWriter = createCsvWriter({
+                    path: 'out.csv',
+                    header: header
+                });
+                csvWriter
+                .writeRecords(data)
+                .then(()=> console.log('The CSV file was written successfully'));
+            });
+        });
+    } else {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+        res.write('<input type="file" name="filetoupload"><br>');
+        res.write('<input type="submit">');
+        res.write('</form>');
+        return res.end();
+    }
+}).listen(process.env.PORT || 3333);
